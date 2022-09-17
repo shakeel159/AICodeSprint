3609dfd9a2f9c4a2a5def5d81941964e3cb17ee9
shakeel159 <jazibshaq@gmail.com>
2022-09-17 12:27:22 -0700

    final return copy

--- AlgorthimsCode/AlgorthimsCode/Program.cs
+++ AlgorthimsCode/AlgorthimsCode/Program.cs
@@ -9,5 +9,37 @@
 
         //constant time
         //O(1) describes an algorithm that will always execute in the same time (or space) regardless of the size of the input data set.
+        public void PrintOneNumber(int[] nums)
+        {
+            Console.Write(nums[0]);
+        }
+
+        //linear time
+        //escribes an algorithm that will always execute in the same time (or space) regardless of the size of the input data set.
+        public void PrintAllNumbers(int[] nums)
+        {
+            foreach (var number in nums)
+            {
+                Console.WriteLine(nums);
+            }
+        }
+
+        //O(n2) represents an algorithm whose performance is directly proportional to the square of the size of the input data set.
+        //This is common with algorithms that involve nested iterations over the data set. 
+
+        public bool FindTheMax(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
     }
 }
