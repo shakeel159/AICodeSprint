d3dc8823b7487b588d3e16b5efe06ad7911259cf
shakeel159 <jazibshaq@gmail.com>
2022-09-17 12:15:56 -0700

    test

---
+++ AlgorthimsCode/AlgorthimsCode/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AlgorthimsCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        //constant time
+        //O(1) describes an algorithm that will always execute in the same time (or space) regardless of the size of the input data set.
+    }
+}
